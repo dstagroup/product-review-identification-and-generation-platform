4a274a1578920519c8353d1e54b88b0e74109e14
Ke_Ren <103575945+ke-ren-heidelberg@users.noreply.github.com>
2023-03-05 11:44:59 +0100

    Update main.py

--- we_recommend-backend/main.py
+++ we_recommend-backend/main.py
@@ -12,16 +12,13 @@ import elasticSearchApi
 
 import pipeline
 
-
-
-# from database import SessionLocal
 import models
 import jwt
 import json
 
 from typing import Optional, Union, List
 
-from pydantic import BaseModel # 引入BaseModel
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -42,9 +39,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# db = SessionLocal()
-
-
 
 class NewUser(BaseModel):
     username: str
@@ -54,37 +48,6 @@ class Query(BaseModel):
     userid: str
     profile: str
 
-# class Cd(BaseModel):
-#     reviews_detail: str
-#
-#
-# class ProductDetail(BaseModel):
-#     brand: str
-#     title: str
-#     description: str
-#     feature_bullets: List[str]
-#
-# class PositiveReviews(BaseModel):
-#     title: str
-#     review: str
-#     rating: int
-#
-# class NegativeReviews(BaseModel):
-#     title: str
-#     review: str
-#     rating: int
-#
-# class Reviews(BaseModel):
-#     positive_reviews: List[PositiveReviews]
-#     negative_reviews: List[NegativeReviews]
-#
-# class OneReviews(BaseModel):
-#     asin: str
-#     product_detail: ProductDetail
-#     reviews: Reviews
-#
-# class ReviewsDetail(BaseModel):
-#     reviews_detail: List[OneReviews]
 class GetAsins(BaseModel):
     username: str
     profile: str
@@ -98,18 +61,6 @@ class Review(BaseModel):
 class ProductReview(BaseModel):
     asin: str
     reviews: List[Review]
-
-# class Reviews(BaseModel):
-#     reviews_detail: List[ProductReview]
-
-# # Dependency
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
 
 
 @app.post('/login')
@@ -131,22 +82,6 @@ def login(user: NewUser):
     if result['statue'] == 13:
         return {"code": "0002", "message": "wrong username or password"}
 
-
-    # db_user = models.User()
-    # db_user.password = hashlib.new('md5', user.password.encode()).hexdigest()
-    # print(db_user.password)
-    # db_user.username = user.username
-    # user = db.query(models.User).filter(models.User.username == user.username, models.User.password == db_user.password).first()
-    # print(user)
-    # if user == None:
-    #     return {"code": "0002", "message": "wrong username or password"}
-    # else:
-    #     encoded_jwt = jwt.encode(data, SECERT_KEY, algorithm=ALGORITHM)
-    #     return{'token': encoded_jwt}
-
-
-# def create_user(user: NewUser, db: Session = Depends(get_db)):
-
 @app.post('/register')
 async def create_user(user: NewUser):
     try:
@@ -158,40 +93,17 @@ async def create_user(user: NewUser):
         print(username)
         print(password)
         es = elasticSearchApi.LoginEsConnect()
-        # es.CreateIndex("account_info")
         es.AddUser(username, password, "account_info")
-
-        # print("aaaaa")
-        # db_user = models.User()
-        # print(user.password)
-        # db_user.password = hashlib.new('md5', user.password.encode()).hexdigest()
-        # db_user.username = user.username
-        # db.add(db_user)
-        # db.commit()
-        # db.refresh(db_user)
     except ArithmeticError:
         return {"code": "0002", "message": "failed"}
 
     return {"code": "0000", "message": "success"}
-
-# @app.put('/reviews')
-# async def get_reviews(reviews_detail: ReviewsDetail):
-#
-#     print(reviews_detail)
-#
-#     if reviews_detail:
-#         return {"code": "0000", "message": "success"}
-#     else:
-#         return {"code": "0002", "message": "failed"}
 
 
 @app.put('/reviews')
 async def get_reviews(request: Request):
 
     data = await request.json()
-    # asin = data.get('reviews_detail')[0].get('asin')
-    # s = json.loads(data);
-    # print(s)
 
     print(data)
     profile = data[0]['profile']
@@ -200,12 +112,6 @@ async def get_reviews(request: Request):
     # profiles = [profile]
 
     es = elasticSearchApi.UsrTableEsConnect()
-    # es.CreateIndex('usr_profile_info')
-    # result = es.AddUserProfile(username, profile,'usr_profile_info')
-    # print(result)
-    #
-    # if result["statue"] == 14 or result["statue"] == 10:
-        # es = elasticSearchApi.CommodityEsConnect()
     for item in data:
 
         print(item)
@@ -216,16 +122,6 @@ async def get_reviews(request: Request):
 
     print("success")
     return {"code": "0000", "message": "success"}
-    # else:
-    #     return {"code": "0002", "message": "failed"}
-
-    # if data:
-    #     return return_json
-    # else:
-    #     return return_json
-
-
-# http://localhost:8895/get_asins?username=abc&profile=cms595619877227
 
 @app.get('/get_asins')
 async def get_analysis(username: str, profile: str):
@@ -250,9 +146,6 @@ async def get_one_asin(profile: str, username: str, asin: str, property: str):
     result = es.QueryCommodity(username, profile, asin, property, -1)
     print(result)
     return result
-
-# request: Request
-# (profile: str, username: str, asins: List[str])
 
 @app.put('/get_analysis_by_profile_userid')
 async def get_analysis_by_profile_userid(request: Request):
@@ -286,96 +179,6 @@ async def get_analysis_by_profile_userid(request: Request):
     print(result_asins)
 
     return json.dumps(result_asins)
-
-# @app.put('/get_analysis_by_profile_userid/{profile}')
-# async def get_analysis_by_profile_userid(profile: str, username: str, asins: List[str]):
-#     es = elasticSearchApi.CommodityEsConnect()
-#     print("sss", profile)
-#     print("hhh", username)
-#
-#
-#     result_asins = []
-#
-#     for asin in asins:
-#         es.QueryCommodityByAsin(username, profile, asin)
-#         all_asins = es.QueryCommodities(username, profile)
-#         for all_asin in all_asins:
-#             all_asin['reviews'] = es.QueryCommodityByAsin(username, profile, asin)
-#
-#         all_rating = 0
-#         post_rating = all_asin['posrating']
-#         for k, v in post_rating.items():
-#             all_rating = all_rating + post_rating[k]
-#         all_asin['all_rating'] = all_rating
-#         print(all_rating)
-#         result_asins.append(all_asin)
-#
-#     indexx = 'abc_cms595619877227_B0BSHDVBXZ'
-#     es = elasticSearchApi.CommodityEsConnect()
-#     result = es.QueryCommodities('abc', 'cms595619877227', 'B0BSHDVBXZ')
-#
-#     print(result_asins)
-#
-#     return json.dumps(result_asins)
-
-
-    # es = elasticSearchApi.CommodityEsConnect()
-    #
-    # for asin in asins:
-    #     es.QueryCommodityByAsin(username, profile)
-    #     all_asins = es.QueryCommodity(username, profile)
-    #     for all_asin in all_asins:
-    #         all_asin['reviews'] = es.QueryCommodityByAsin(username, profile, asin)
-    #
-    # return all_asin
-
-# @app.put('/get_analysis_by_profile_userid/{profile}')
-# async def get_analysis_by_profile_userid(profile: str, username: str):
-#     es = elasticSearchApi.CommodityEsConnect()
-#     es2 = elasticSearchApi.UsrTableEsConnect()
-#     profiles = es2.GetUsrProfile(username, 'usr_profile_info')
-#     for profile in profiles[1]:
-#         es.QueryCommodity(username, profile, )
-#     es.QueryCommodity()
-
-@app.get('/asss')
-async def get_revisews():
-    asins = ['B0BSHDVBXZ']
-    username = 'abc'
-    profile = 'cms595619877227'
-    es = elasticSearchApi.CommodityEsConnect()
-
-    result_asins = []
-
-    for asin in asins:
-        es.QueryCommodityByAsin(username, profile, asin)
-        all_asins = es.QueryCommodities(username, profile)
-        for all_asin in all_asins:
-            all_asin['reviews'] = es.QueryCommodityByAsin(username, profile, asin)
-
-
-        all_rating = 0
-        post_rating = all_asin['posrating']
-        for k,v in post_rating.items():
-            all_rating = all_rating + post_rating[k]
-        all_asin['all_rating'] = all_rating
-        print(all_rating)
-        result_asins.append(all_asin)
-
-    #
-    # indexx = 'abc_cms595619877227_B0BSHDVBXZ'
-    # es = elasticSearchApi.CommodityEsConnect()
-    # result = es.QueryCommodities('abc', 'cms595619877227', 'B0BSHDVBXZ')
-
-    for i in range(len(result_asins)):
-        for j in range(i + 1, len(result_asins)):
-            if result_asins[i]['all_rating'] > result_asins[j]['all_rating']:
-                result_asins[i], result_asins[j] = result_asins[j], result_asins[i]
-
-    print(result_asins)
-
-    return json.dumps(result_asins)
-
 
 
 
